69ed74d6e979965d31bdc471e9d6d609fb95e9f4
Yt-trium <cyril.meyer.68@gmail.com>
2016-08-05 01:00:49 +0200

    Update sql db export

--- hgpos.sql
+++ hgpos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 01 Août 2016 à 23:52
+-- Généré le :  Jeu 04 Août 2016 à 22:59
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -48,7 +48,8 @@ INSERT INTO `caisse` (`idCaisse`, `date`, `totalTheorie`, `totalReel`, `erreurSt
 (7, '2016-07-31 00:58:57', 10.5, 10.6, 0, 0),
 (8, '2016-07-31 01:02:04', 0, 0, 0, 0),
 (9, '2016-07-31 01:09:56', 10, 15, 4, 1),
-(10, '2016-08-01 19:41:58', 11.5, 13, 1.2, 0.3);
+(10, '2016-08-01 19:41:58', 11.5, 13, 1.2, 0.3),
+(11, '2016-08-03 18:07:01', 20, 20.5, 0.5, 0);
 
 -- --------------------------------------------------------
 
@@ -70,7 +71,9 @@ CREATE TABLE `membres` (
 
 INSERT INTO `membres` (`idMembre`, `nom`, `prenom`, `email`, `annee`) VALUES
 (3, 'M', 'Cyril', 'cyril@m.m', 31),
-(4, 'ab', 'bc', 'de', 100);
+(4, 'ab', 'bc', 'de', 100),
+(5, 'ab', 'bc', 'azrazijroaizjr', 1),
+(6, 'Meyer', 'Bernard', 'meyer.bernard@toto.fr', 100);
 
 -- --------------------------------------------------------
 
@@ -118,16 +121,39 @@ CREATE TABLE `stock` (
 --
 
 INSERT INTO `stock` (`idProduit`, `quantite`) VALUES
-(1, 28),
-(2, 31),
-(3, 43),
+(1, 27),
+(2, 30),
+(3, 42),
 (4, 44),
 (5, 44),
-(6, 46),
+(6, 45),
 (7, 40),
 (8, 48),
-(9, 47),
-(10, 49);
+(9, 44),
+(10, 47);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `idUtilisateur` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `droitAcces` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`idUtilisateur`, `login`, `password`, `droitAcces`) VALUES
+(1, 'tresorier', 'tresorier', 10),
+(2, 'stock', 'stock', 5),
+(3, 'president', 'president', 20),
+(4, 'root', 'root', 42);
 
 -- --------------------------------------------------------
 
@@ -191,7 +217,9 @@ INSERT INTO `vente` (`idVente`, `idProduit`, `date`, `unite`) VALUES
 (42, 1, '2016-08-01 19:44:51', 1),
 (43, 9, '2016-08-01 19:44:51', 1),
 (44, 6, '2016-08-01 19:44:51', 1),
-(45, 10, '2016-08-01 22:54:20', 1);
+(45, 10, '2016-08-01 22:54:20', 1),
+(46, 10, '2016-08-02 00:16:41', 1),
+(47, 9, '2016-08-03 18:04:03', 2);
 
 --
 -- Index pour les tables exportées
@@ -222,6 +250,13 @@ ALTER TABLE `stock`
   ADD PRIMARY KEY (`idProduit`);
 
 --
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`idUtilisateur`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
 -- Index pour la table `vente`
 --
 ALTER TABLE `vente`
@@ -235,22 +270,27 @@ ALTER TABLE `vente`
 -- AUTO_INCREMENT pour la table `caisse`
 --
 ALTER TABLE `caisse`
-  MODIFY `idCaisse` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idCaisse` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `membres`
 --
 ALTER TABLE `membres`
-  MODIFY `idMembre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idMembre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `produits`
 --
 ALTER TABLE `produits`
   MODIFY `idProduit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT pour la table `vente`
 --
 ALTER TABLE `vente`
-  MODIFY `idVente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `idVente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
